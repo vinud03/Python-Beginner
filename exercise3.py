783c64b63b6ec3fe6424ff9532f1c220c864b287
Vinayak Dagade <vinud03@users.noreply.github.com>
2021-01-15 18:03:08 +0530

    Guess the number
    This is simple code for the game "Guess the number". User can guess the number in 5 attempt. Here the number have range from 1-15. User have 5 attempt to guess the number.

---
+++ exercise3.py
@@ -0,0 +1,31 @@
+def guess():
+    num = RAnd_num.randint(1, 15)
+    print("Guess the number from 1 to 15\n Total number of attempts 5")
+    print('Enter the Number you guess')
+    for i in range(1, 6):
+        num2 = int(input())
+        if num2 > num:
+            print('Enter number is greater\n Try again')
+        elif num2 < num:
+            print('Enter number is smaller\n Try again')
+        elif num2 == num and i <= 5:
+            print('Congratulation you guess correctly in %d attempt' % i)
+            break
+        elif (i == 5 and num2 != num):
+            print("The correct answer is:%d" % num)
+        else:
+            print('Guess again\n %d attempt left' % (5 - i))
+        i = i + 1
+
+
+import RAnd_num
+
+
+while(True):
+    guess()
+    print("Press Y to play again\nOR\nPress any key ")
+    k=input()
+    if k=='Y' or k=='y':
+        guess()
+    else:
+        break
